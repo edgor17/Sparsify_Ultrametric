8d5d323b28dfb37e45948ab9dd4b813aaa652f10
edgor17 <87628022+edgor17@users.noreply.github.com>
2023-05-17 17:24:03 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(
-    name='Sparsify_Ultrametric',
+    name='SparsifyUltrametric',
     url='https://github.com/edgor17/Sparsify-Ultrametric',
     author='Evan Gorman',
     author_email='evan.gorman@colorado.edu',
-    packages=['Sparsify_Ultrametric'],
+    packages=['SparsifyUltrametric'],
     install_requires=['numpy','pandas','ete3','scipy','matplotlib'],
     version='0.1',
     description='Sparsification of Strictly Ultrametric Matrices',
